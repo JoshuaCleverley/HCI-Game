81927a6188d9c02ae87bd40a1b5449cde9fdbbb6
Joshua Cleverley <JoshuaCleverley@protonmail.com>
2020-02-25 16:16:05 +0000

    Added questions.js

---
+++ questions.js
@@ -0,0 +1,16 @@
+// Setup questions
+let literacyQuestions = [
+  new Question("a doing word", "verb"),
+  new Question("a describing word", "adjective"),
+  new Question("the opposite of up", "down"),
+  new Question("not right", "left"),
+  new Question("vowels", "aeiou"),
+]
+
+let numeracyQuestions = [
+  new Question("5 + 5", "10"),
+  new Question("6 * 2", "12"),
+  new Question("5 * 1", "5"),
+  new Question("5 / 2", "2.5"),
+  new Question("3^3", "27"),
+]
